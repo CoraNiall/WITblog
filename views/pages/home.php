a8184365526cf13bca34d1d4b5c83ebfe6a5a215
LauraRowlands <laura.rowlands02@gmail.com>
2019-04-22 23:30:37 +0100

    Update home.php
    Changes to home to get the guest welcome or username once logged in

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,3 +1,4 @@
-<p>Hello there <?php echo $_SESSION['username']; ?>!<p>
+<p>Hello there <?php if (!empty ($_SESSION)){echo $_SESSION['username']; }
+                    else {echo "guest";} ?>!<p>
 <p>This is now the home page for the W.I.T Final Project Blog</p>
 <p>This is an adaptation of the MVC Skeleton Application</p>
