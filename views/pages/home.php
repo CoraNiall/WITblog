efb64eb314cd560301da9b0177c4c8bbbca36654
LauraRowlands <laura.rowlands02@gmail.com>
2019-04-22 23:18:25 +0100

    Update home.php
    Updated files to include a working sewssion for our registered users! to geth this to work properly right now, you do need to be logged in as a registered user...

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,3 +1,3 @@
-<p>Hello there <?php echo $first_name . ' ' . $last_name; ?>!<p>
+<p>Hello there <?php echo $_SESSION['username']; ?>!<p>
 <p>This is now the home page for the W.I.T Final Project Blog</p>
 <p>This is an adaptation of the MVC Skeleton Application</p>
