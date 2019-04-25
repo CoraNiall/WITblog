172f434fd0d83f3ec4245278d2da1e9576a96d10
LauraRowlands <laura.rowlands02@gmail.com>
2019-04-25 22:20:45 +0100

    Update editProfile.php
    Changes to the profile picture

--- views/login/editProfile.php
+++ views/login/editProfile.php
@@ -1,6 +1,20 @@
+<link rel = "stylesheet" type = "text/css" href = "views/css/styles.css" />
+
 <p>Fill in the following form to update your profile information:</p>
 <form action="" method="POST" class="w3-container" enctype="multipart/form-data">
     <h2>Update Profile</h2>
+    <?php 
+$file = 'views/images/' . $user->username . '.jpeg';
+if(file_exists($file)){
+    $img = "<img src='$file' class='pfphoto' width='150' />";
+    echo $img;
+}
+else
+{
+echo "<img src='views/images/standard/_noproductimage.png' width='150' />";
+}
+
+?>
     <p>
         <label>Email</label>
         <input class="w3-input" type="text" name="email" value="<?= $user->email; ?>">
@@ -15,18 +29,7 @@
     </p>
             
   <input type="hidden" name="MAX_FILE_SIZE" value="10000000" />
-<?php 
-$file = 'views/images/' . $user->username . '.jpeg';
-if(file_exists($file)){
-    $img = "<img src='$file' width='150' />";
-    echo $img;
-}
-else
-{
-echo "<img src='views/images/standard/_noproductimage.png' width='150' />";
-}
 
-?>
   <br/>
   <input type="file" name="myUploader" class="w3-btn w3-pink" />
   <p>
