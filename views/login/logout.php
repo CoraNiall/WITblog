e950b3909cf608da973f7dff188cb69c0ac15702
LauraRowlands <laura.rowlands02@gmail.com>
2019-04-25 22:20:56 +0100

    Update logout.php
    added css links

--- views/login/logout.php
+++ views/login/logout.php
@@ -1,3 +1,5 @@
+<link rel = "stylesheet" type = "text/css" href = "views/css/styles.css" />
+
 <p>Thanks for your visit.<br>You have now been logged out.</p>
 <p><a href='?controller=pages&action=home' class="btn btn-warning">Go To Homepage</a>
     <a href='?controller=login&action=login' class="btn btn-danger">Log In</a></p>
