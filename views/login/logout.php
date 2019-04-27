6980578e18d9212ccd15712f6d0715caa73ef1f5
CarolinaReiss <cl.reiss@gmail.com>
2019-04-27 15:08:43 +0100

    Updates to layouts for user profile and css additions

--- views/login/logout.php
+++ views/login/logout.php
@@ -2,7 +2,7 @@
 
 
 <div class='logout'>
-    <p>Thanks for your visit.<br>You have now been logged out.</p>
+    <H2>Thanks for your visit.</h2><h3>You have now been logged out.</h3>
     <p><a href='?controller=pages&action=home' class="btn btn-warning">Go To Homepage</a></p>
     <p><a href='?controller=login&action=login' class="btn btn-danger">Log In</a></p>
 </div>
