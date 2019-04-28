5377dac04320cf326d4aea4b39d8358db14720c9
Jessie Stuart <jessiestuart@hotmail.co.uk>
2019-04-28 13:17:36 +0100

    Bug fix for image upload errors

--- views/posts/create.php
+++ views/posts/create.php
@@ -1,14 +1,24 @@
 <head>
-<link rel = "stylesheet" type = "text/css" href = "views/css/styles.css" />
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" type = "text/css" href = "views/css/styles.css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 
 <form action="" method="POST" class="w3-container" enctype="multipart/form-data">
 
 
-    <center>    <h2>Add New Post</h2> <br>
+    <center>    
+
+        <h2>Add New Post</h2>
+        <p>
+            <?php
+            if (isset ($error) && $error ) {
+                echo $error;
+            }
+            ?>
+        </p>
+        <br>
         <p>
             <label>Post Title</label> <br>
             <textarea rows="1" cols="70" <input class="w3-input" type="text" name="title" required autofocus> </textarea> </br>
@@ -24,18 +34,18 @@
         <div class="row">
             <div>
 
-            <div class ="form-group">
-                <label for="sel2">Tags</label> <br>
-                <select name="tag[]" multiple="multiple" class="form-control" id="sel2" required="required">
-                    <?php
-                    foreach ($tags as $tag) {
-                        echo "<option value=" . $tag->id . ">" . $tag->tag . "</option>";
-                    }
-                    ?>
-                </select>
-                
+                <div class ="form-group">
+                    <label for="sel2">Tags</label> <br>
+                    <select name="tag[]" multiple="multiple" class="form-control" id="sel2" required="required">
+                        <?php
+                        foreach ($tags as $tag) {
+                            echo "<option value=" . $tag->id . ">" . $tag->tag . "</option>";
+                        }
+                        ?>
+                    </select>
+
+                </div>
             </div>
-        </div>
         </div>
         <br>     
         <input type="hidden" 
@@ -43,10 +53,10 @@
                value="10000000"
                />
         <label>Select Images to upload:</label>
-        
-  <input type="file" name="myUploader" class="w3-btn w3-pink" required="required" />
-  <p>
-       <!-- <input type="file" name="files[]" multiple="multiple" class="w3-btn w3-pink" />  -->
+
+        <input type="file" name="myUploader" class="w3-btn w3-pink" required="required" />
+        <p>
+             <!-- <input type="file" name="files[]" multiple="multiple" class="w3-btn w3-pink" />  -->
         <p>
             <br>
             <input class="w3-btn w3-pink" type="submit" value="Add Post">
